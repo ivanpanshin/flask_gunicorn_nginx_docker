eafa9377c9e0bda4c559c0d695a7a17944bf4b8a
Ivan Panshin <48650449+ivanpanshin@users.noreply.github.com>
2020-04-30 02:17:33 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request, jsonify
+from random import sample
+
+
+server = Flask(__name__)
+
+def run_request():
+    index = int(request.json['index'])
+    list = ['red', 'green', 'blue', 'yellow', 'black']
+    return list[index]
+
+@server.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'GET':
+        return 'The model is up and running. Send a POST request'
+    else:
+        return run_request()
+
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from app import server
+
+if __name__ == "__main__":
+    server.run(host='0.0.0.0', port=8000)
+

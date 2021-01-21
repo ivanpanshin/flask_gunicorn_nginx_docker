110a221702bacd3836fb3c823257dfb527ea46c0
Christopher Goddard <chrisjgoddard@gmail.com>
2021-01-21 16:35:11 +1100

    Add new route to flask app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request, jsonify
 from random import sample
+import subprocess
 
 server = Flask(__name__)
 
@@ -9,6 +10,14 @@ def run_request():
     list = ['red', 'green', 'blue', 'yellow', 'black']
     return list[index]
 
+def run_faas_request():
+    function = request.json['function']
+    # ref xspdf.com/resolution/50942456.html
+    process = subprocess.Popen(['export', 'OPENFAAS_URL=https://faasd.cthulu.tk', '&&', 'echo', '|', 'faas-cli', 'invoke', function], stdout=subprocess.PIPE)
+    stdout = process.communicate()[0]
+    return 'STDOUT:{}'.format(stdout)
+
+
 @server.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'GET':
@@ -16,3 +25,9 @@ def hello_world():
     else:
         return run_request()
 
+@server.route('/faas', methods=['GET', 'POST'])
+def make_faas_request():
+    if request.method == 'GET':
+        return 'This is the faas route.  Send a POST request with json payload of the form { function: catapi }.'
+    if request.method == 'POST':
+        return run_faas_request()
